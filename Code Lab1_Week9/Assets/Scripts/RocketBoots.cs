7c3104acf2521fc7e526f396cf617c9c7e95cc11
Unknown <paosalcedo@gmail.com>
2017-04-04 19:16:48 -0400

    Bridge Boots now have limited ammo.

--- Code Lab1_Week9/Assets/Scripts/RocketBoots.cs
+++ Code Lab1_Week9/Assets/Scripts/RocketBoots.cs
@@ -4,14 +4,20 @@ using UnityEngine;
 
 public class RocketBoots : BasicBoots {
 
+	int usesLeft;
+
+	void Start(){
+		usesLeft = 1;
+	}
 	// Use this for initialization
 	public override void ActivateBoots (Vector3 pos, float offsetX, float offsetY, float offsetZ, int uses)
 	{
-		if (uses > 0) {
+		if (usesLeft > 0) {
 			print ("Jumped with rocket boots");
 			GameObject rocket = Instantiate (Resources.Load ("Prefabs/Rocket") as GameObject);
 			rocket.transform.position = pos;
 			rocket.transform.position = new Vector3 (pos.x + offsetX, pos.y + offsetY, pos.z + offsetZ);
+			usesLeft -= 1;
 			uses -= 1;
 			print("Number of uses remaining: " + uses);
 		}
